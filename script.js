09d22566cac24ab5ca03b9c29c6b8d111a58d183
Andrew Taylor <andrew.taylor04@bbc.co.uk>
2016-06-23 13:43:59 +0100

    Added colours for the groupings

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var n = 2000,
+var n = 1000,
 	distance = 0.5,
 	points = [ { x: 50, y: 20 }, { x: 18, y: 80 }, { x: 82, y: 80 } ],
 	//points = [ { x: 10, y: 10 }, { x: 10, y: 90 }, { x: 90, y: 10 }, { x: 90, y: 90 } ],
 	dots = [],
-	field;
+	field,
+	moveTimeout = 1000, // todo: this should really be in css
+	moveLength = 2000;
 
 document.addEventListener('DOMContentLoaded', function(e) {
 	field = document.getElementById('field');
@@ -17,33 +19,35 @@ document.addEventListener('DOMContentLoaded', function(e) {
 			y: random() * 100
 		};
 	}
-	points.forEach(function(point) {
+	points.forEach(function(point, i) {
 		point.element = document.createElement('div');
 		point.element.classList.add('point');
+		point.element.classList.add('p' + i);
 		field.appendChild(point.element);
 		position(point);
 	});
-	step();
-	field.addEventListener('click', step);
+	dots.forEach(position);
+	window.addEventListener('click', step);
 });
 
 function step() {
 	dots.forEach(function(dot) {
-		dot.element.style.left = dot.x + '%';
-		dot.element.style.top = dot.y + '%';
-		point = pick(points);
-		dot.x = dot.x * (1 - distance) + point.x * distance;
-		dot.y = dot.y * (1 - distance) + point.y * distance;
+		var i = ~~(Math.random() * points.length);
+		dot.element.classList.add('p' + i);
+		setTimeout(function() {
+			dot.x = dot.x * (1 - distance) + points[i].x * distance;
+			dot.y = dot.y * (1 - distance) + points[i].y * distance;
+			position(dot);
+		}, moveTimeout);
+		setTimeout(function() {
+			dot.element.classList.remove('p' + i);
+		}, moveTimeout + moveLength);
 	});
 }
 
 function position(dot) {
 	dot.element.style.left = dot.x + '%';
 	dot.element.style.top = dot.y + '%';
-}
-
-function pick(arr) {
-	return arr[~~(Math.random() * arr.length)];
 }
 
 function random() {
